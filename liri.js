e84a6b6496f7f885bb0ce74482da897b11b35b16
priscila <pristiezzi@gmail.com>
2019-07-07 19:17:08 -0500

    need to be finalized

--- liri.js
+++ liri.js
@@ -1,17 +1,20 @@
 var axios = require ('axios');
 var command = process.argv[2]
 var Spotify = require('node-spotify-api');
-// var request = require("request");
- var input = process.argv[3];
-// var keys = require("./keys.js");
-// var fs = require("fs");
-// var spotify = new Spotify(keys.spotify);
+var request = require("request");
+var input = process.argv[3];
+var keys = require("./keys.js");
+var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
 
-if (command==="spotify-this-song"){
-    spotifyThisSong()
-} else if (command==="concert-this"){
-    
+if(command === "spotify-this-song") {
+  spotifyThisSong(input);
+} else if(command === "concert-this"){
+  concertThis(input)
+} else if (command === "movie-this"){
+  movieThis(input)
 }
+
 
 
 function spotifyThisSong (songName){
@@ -33,71 +36,77 @@ function spotifyThisSong (songName){
    });
 }
 
-// handles commands
-if(command === "spotify-this-song") {
-  spotifyThisSong(input);
-} else if(command === "concert-this"){
-  concertThis(input)
-} else if (command === "movie-this"){
-  movieThis(input)
-}
+// // make a function for each command
+//   function concertThis (songName){
+//   if(!songName){
+//       songName = "Sucker";
+//    }
+//   // bands in town code
+// }
+// // Optionally the request above could also be done as
+// axios.get('/user', {
+//   params: {
+//     ID: 12345
+//   }
+// })
+// .then(function (response) {
+//   console.log(response);
+// })
+// .catch(function (error) {
+//   console.log(error);
+// })
+// .then(function () {
+//   // always executed
+// });  
 
-// make a function for each command
-function spotifyThisSong(songName) {
-  // spotify code
-}
+  this.concert-this + function (band) {
+    var URL ="http//rest.bandsintown.com/artists" + artist + "events?app_id=codingbootcamp"
 
-function concertThis(artist) {
-  // bands in town code
-}
-// Optionally the request above could also be done as
-axios.get('/user', {
-  params: {
-    ID: 12345
-  }
-})
-.then(function (response) {
-  console.log(response);
-})
-.catch(function (error) {
-  console.log(error);
-})
-.then(function () {
-  // always executed
-});  
+    axios.get(URL).then(function (response) {
 
+      var jsonData = response.data;
+      console.log(jsonData)
+      var showData = [
+        "Venue name: " + jsonData.name,
+        "Venue location: " + jsonData.location,
+        "Date : " + jsonData.Date.event,
+      ].join("\n\n");
 
-// var axios = require("axios");
-// var fs = require("fs");
+      // Append showData and the divider to log.txt, print showData to the console
 
-// var TV = function () {
-//   var divider = "\n-----\n\n";
-
-
-//   this.findShow = function (show) {
-//     var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
-
-//     axios.get(URL).then(function (response) {
-
-//       var jsonData = response.data;
-//       console.log(jsonData)
-//       var showData = [
-//         "Show: " + jsonData.name,
-//         "Genre(s): " + jsonData.genres,
-//         "Rating: " + jsonData.rating.average,
-//         "Network: " + jsonData.network,
-//         "Summary: " + jsonData.summary
-//       ].join("\n\n");
-
-//       // Append showData and the divider to log.txt, print showData to the console
-
-//       fs.appendFile('message.txt', showData, function (err) {
-//         if (err) throw err;
-//         console.log('Saved!');
-//       });
+      fs.appendFile('message.txt', venueta, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
 
       
-//     })
-//   }
-// }
-// module.exports = TV;
+    })
+  }
+
+this.movie-this + function(movieThis) {
+  var URL = "http://www.imdb.com/title/tt0485947/" + Mr.Nobody;
+
+  axios.get(URL).then(function(response) {
+    var jsonData = response.data[0].person;
+    var actorData = [
+      "Year: " + jsonData.year,
+      "IMDB rating: " + jsonData.imdb.rating,
+      "Language: " + jsonData.language,
+      "Country: " + jsonData.country.name,
+      "URL: " + jsonData.url,
+      "Actors: " + jasonData.actors,
+      "Plot: " + jasonData.plod,
+      "Rotten tomatoes" + jasonData.rotten.tomatoes,
+    
+    ].join("\n\n");
+
+    
+    fs.appendFile("log.txt", actorData + divider, function(err) {
+      if (err) throw err;
+      console.log(actorData);
+
+    });
+  });
+};
+
+
